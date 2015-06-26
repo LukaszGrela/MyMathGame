922c771c66b1212a5507ecefb484be4280dc92fd
Łukasz 'Severiaan' Grela <lukasz.grela@gmail.com>
2015-06-26 13:44:35 +0100

    Work in progress Allow second try text externalised New Change button for number range, now separate for +,- and *,/ operations New layout for number range

--- app/src/main/java/com/greladesign/examples/games/mymathgame/MainActivity.java
+++ app/src/main/java/com/greladesign/examples/games/mymathgame/MainActivity.java
@@ -3,9 +3,12 @@ package com.greladesign.examples.games.mymathgame;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.greladesign.examples.games.mymathgame.game.Operation;
 
@@ -14,10 +17,24 @@ import java.util.EnumSet;
 
 public class MainActivity extends Activity {
 
+
+    private static final String TAG = "MainActivity";
     private Button btnGameAddition;
     private Button btnGameSubstraction;
     private Button btnGameMultiplication;
     private Button btnGameDivision;
+    private View.OnClickListener mRangeSelectionListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.i(TAG, "Click");
+            if (v == mChangeAddSubRange) {
+                Log.i(TAG, "Changing the adding/subtracting range");
+            }
+            if (v == mChangeMulDivRange) {
+                Log.i(TAG, "Changing the multiply/division range");
+            }
+        }
+    };
     private View.OnClickListener mSelectGameListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -38,6 +55,10 @@ public class MainActivity extends Activity {
         }
     };
     private CheckBox mCbAllowSecondTry;
+    private TextView mTvAddSubRangeDescription;
+    private TextView mTvMulDivRangeDescription;
+    private TextView mChangeAddSubRange;
+    private TextView mChangeMulDivRange;
 
     private void startNewGame(int range, EnumSet<Operation> operationId) {
         final Intent gameIntent = new Intent(this, GameActivity.class);
@@ -63,10 +84,23 @@ public class MainActivity extends Activity {
         //
         mCbAllowSecondTry = (CheckBox) findViewById(R.id.cbAllowSecondTry);
         //
+        LinearLayout mAddSubRangeLayout = (LinearLayout) findViewById(R.id.llRangeAddSub);
+        if(mAddSubRangeLayout != null) {
+            mTvAddSubRangeDescription = (TextView) mAddSubRangeLayout.findViewById(R.id.tvDetails);
+            mChangeAddSubRange = (TextView) mAddSubRangeLayout.findViewById(R.id.tvChangeBtn);
+            mChangeAddSubRange.setOnClickListener(mRangeSelectionListener);
+        }
+        LinearLayout mMulDivRangeLayout = (LinearLayout) findViewById(R.id.llRangeMulDiv);
+        if(mMulDivRangeLayout != null) {
+            mTvMulDivRangeDescription = (TextView) mMulDivRangeLayout.findViewById(R.id.tvDetails);
+            mChangeMulDivRange = (TextView) mMulDivRangeLayout.findViewById(R.id.tvChangeBtn);
+            mChangeMulDivRange.setOnClickListener(mRangeSelectionListener);
+        }
         //add logic
         btnGameAddition.setOnClickListener(mSelectGameListener);
         btnGameSubstraction.setOnClickListener(mSelectGameListener);
         btnGameMultiplication.setOnClickListener(mSelectGameListener);
         btnGameDivision.setOnClickListener(mSelectGameListener);
+
     }
 }
